289faf74b4fd62d5f8678a7aaf5c78ada281eb3a
Mostafa <105555194+mostafaamahmoudd@users.noreply.github.com>
2025-12-15 14:51:41 +0200

    [412] [Time Beats: 100.00%] [Memory Beats: 68.57%] - LeetPush

--- cpp/Fizz-Buzz.cpp
+++ cpp/Fizz-Buzz.cpp
@@ -3,16 +3,12 @@
 3    vector<string> fizzBuzz(int n) {
 4        vector<string> v;
 5        for (int i = 1; i <= n; i++) {
-6            if (i % 3 == 0 && i % 5 == 0)
-7                v.push_back("FizzBuzz");
-8            else if (i % 3 == 0)
-9                v.push_back("Fizz");
-10            else if (i % 5 == 0)
-11                v.push_back("Buzz");
-12            else
-13                v.push_back(to_string(i));
-14        }
-15
-16        return v;
-17    }
-18};
+6            v.push_back((i % 15 == 0)  ? "FizzBuzz"
+7                        : (i % 5 == 0) ? "Buzz"
+8                        : (i % 3 == 0) ? "Fizz"
+9                                       : to_string(i));
+10        }
+11
+12        return v;
+13    }
+14};
